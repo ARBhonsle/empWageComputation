e1bd54dcf22ba310c0aed60ee2c62bfd95c29525
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-04 22:01:10 +0530

    employee attendance added in empWageCompute.sh

--- empWageCompute.sh
+++ empWageCompute.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
+empPresent=$((RANDOM%2))
+if [ $empPresent -eq 1 ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
