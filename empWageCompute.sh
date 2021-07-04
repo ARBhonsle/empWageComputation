9ef5c50d923042fa5a127b2085abc6102330f2ab
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-04 22:14:05 +0530

    employee wage calculated in empWageCompute.sh

--- empWageCompute.sh
+++ empWageCompute.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
 empPresent=$((RANDOM%2))
+wagePerHr=20
 if [ $empPresent -eq 1 ]
 then
 	echo "Employee Present"
+	HrsCalculated=8
 else
 	echo "Employee Absent"
+	HrsCalculated=0
 fi
+salary=$(($wagePerHr*$HrsCalculated))
+echo "Wage per Hr = $wagePerHr Hrs calculated = $HrsCalculated"
+echo "Salary = $salary"
