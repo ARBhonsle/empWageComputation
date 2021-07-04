1bb9adf06f57fc1917415f5c7a1e370e5992cc20
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-04 22:28:03 +0530

    wage calculation by switch case

--- empWageCompute.sh
+++ empWageCompute.sh
@@ -2,18 +2,29 @@
 echo "Welcome to Employee Wage Computation Program"
 empPresent=$((RANDOM%3))
 wagePerHr=20
-if [ $empPresent -eq 2 ]
-then
-	echo "Employee Present"
-	HrsCalculated=8
-elif [ $empPresent -eq 1 ]
-then
-	echo "Employee Part-time"
-	HrsCalculated=8
-else
-	echo "Employee Absent"
-	HrsCalculated=0
-fi
-salary=$(($wagePerHr*$HrsCalculated))
-echo "Wage per Hr = $wagePerHr Hrs calculated = $HrsCalculated"
+case $empPresent in
+	2) echo "Employee Present"
+	   hrsCalculated=8
+		;;
+	1) echo "Employee Part-Time"
+	   hrsClaculated=8
+		;;
+	0) echo "Employee Absent"
+	   hrsCalculated=0
+		;;
+esac
+#if [ $empPresent -eq 2 ]
+#then
+#	echo "Employee Present"
+#	HrsCalculated=8
+#elif [ $empPresent -eq 1 ]
+#then
+#	echo "Employee Part-time"
+#	HrsCalculated=8
+#else
+#	echo "Employee Absent"
+#	HrsCalculated=0
+#fi
+salary=$(($wagePerHr*$hrsCalculated))
+echo "Wage per Hr = $wagePerHr, Hrs calculated = $hrsCalculated"
 echo "Salary = $salary"
