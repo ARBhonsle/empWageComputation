4cbe4e12f5a7308b15762719527e7c4341477d32
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-04 23:11:56 +0530

    refactor:function to get work hrs in empWageCompute.sh

--- empWageCompute.sh
+++ empWageCompute.sh
@@ -1,36 +1,35 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
+function workHrsCalculation()
+{
+	totWorkingHr=$1
+	hrsCalc=0
+	for((day=0;day<20 && $hrsCalc<$totWorkingHr;day++))
+	do
+		empPresent=$((RANDOM%3))
+		case $empPresent in
+			2) #echo "Employee Present"
+			   hrsCalc=$(($hrsCalc+8))
+				;;
+			1) #echo "Employee Part-Time"
+			   hrsCalc=$(($hrsCalc+8))
+				;;
+			0) #echo "Employee Absent"
+			   hrsCalc=$(($hrsCalc+0))
+				;;
+		esac
+	done
+	echo "Days worked=$day"
+	return $hrsCalc
+}
+
 wagePerHr=20
-totWorkingHr=100
-hrsCalculated=0
-for((day=1;day<=20 && $hrsCalculated<$totWorkingHr;day++))
-do
-	empPresent=$((RANDOM%3))
-	case $empPresent in
-		2) #echo "Employee Present"
-		   ((hrsCalculated+=8))
-			;;
-		1) #echo "Employee Part-Time"
-		   ((hrsClaculated+=8))
-			;;
-		0) #echo "Employee Absent"
-		   ((hrsCalculated+=0))
-			;;
-	esac
-done
-#if [ $empPresent -eq 2 ]
-#then
-#	echo "Employee Present"
-#	HrsCalculated=8
-#elif [ $empPresent -eq 1 ]
-#then
-#	echo "Employee Part-time"
-#	HrsCalculated=8
-#else
-#	echo "Employee Absent"
-#	HrsCalculated=0
-#fi
-salary=$(($wagePerHr*$hrsCalculated))
+hrs=100
 echo "Wage per Hr = $wagePerHr"
-echo "Days worked=$(($day-1)), Hrs calculated = $hrsCalculated"
+
+workHrsCalculation $hrs
+hrsCalc=$?
+echo "Hrs calculated = $hrsCalc"
+
+salary=$(($wagePerHr*$hrsCalc))
 echo "Salary = $salary"
